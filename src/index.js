5e9eef8aece7e221cf71ede247918d65e4b92959
Barnabas Bulpett <barnabasbulpett@gmail.com>
2017-08-19 19:09:24 -0400

    Basic component setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDj3JtxG-XjdOFyOKmfG_TlEywTCYt7K9s';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component. This component should produce some HTML
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {       // <- App is a class of component
+  return <div>Hi!</div>;  // <- that is JSX
+}
+// ^^^ Const is an ES6 (ES2016) thing. Declares variable similar to var. It is a "constant", will never change.
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
